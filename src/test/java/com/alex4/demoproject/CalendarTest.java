fd27aeda26e8135ac2d50c40656150b1403a2410
AC51014 <alexey.chekulaev@citi.com>
2019-01-01 17:58:59 -0500

    cleanup maven dependencies

--- src/test/java/com/alex4/demoproject/CalendarTest.java
+++ src/test/java/com/alex4/demoproject/CalendarTest.java
@@ -2,20 +2,12 @@ package com.alex4.demoproject;
 
 import org.junit.Test;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
-
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.nullValue;
 
 public class CalendarTest {
     @Test
     public void should1() {
-    }
-
-    @Test
-    public void should2() {
+        assertThat(1 , is(1));
     }
 }
